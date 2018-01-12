359cf6e7f6d7aade16428bde38a643ea3d732d7b
niilka <daemon.named@gmail.com>
2018-01-12 18:14:52 +0300

    ready

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,7 +1,12 @@
 const request = new XMLHttpRequest();
-request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', false);
+
+request.addEventListener('load', updateWeather);
+request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather');
 request.send();
-if (request.status === 200) {
-  const response = JSON.parse(request.responseText);
-  setData(response);
+
+function updateWeather(event) {
+  if (event.target.status === 200) {
+    const response = JSON.parse(event.target.responseText);
+    setData(response);
+  }
 }
